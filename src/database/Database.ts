022ce234a976ee4e2a14db74bf2daea4c296c59b
Mopcho <47301161+Mopcho@users.noreply.github.com>
2023-10-22 13:33:38 +0300

    Create some more database functions for the users

--- src/database/Database.ts
+++ src/database/Database.ts
@@ -18,9 +18,37 @@ export class Database {
         });
     }
 
+    async findUnique(searchData?: Prisma.UserWhereUniqueInput) {
+        return prisma.user.findFirst({
+            where: searchData,
+        });
+    }
+
     async findUsers(searchData?: Prisma.UserWhereInput) {
         return prisma.user.findMany({
             where: searchData
+        })
+    }
+
+    async updateUser(searchData: Prisma.UserWhereUniqueInput, updateData: Prisma.UserUpdateInput) {
+        return prisma.user.update({
+            where: searchData,
+            data: updateData
+        })
+    }
+
+    async deleteUser(searchData: Prisma.UserWhereUniqueInput) {
+        return prisma.user.delete({
+            where: searchData,
+        })
+    }
+
+    async softDeleteUser(searchData: Prisma.UserWhereUniqueInput) {
+        return prisma.user.update({
+            where: searchData,
+            data: {
+                status: 'DELETED'
+            }
         })
     }
 }
