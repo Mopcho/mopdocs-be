2b98f0df82d896365a334a87297e9742fdd9c638
Mopcho <47301161+Mopcho@users.noreply.github.com>
2023-10-31 11:31:59 +0200

    Log unhandled errros

--- src/api/middlewares/GlobalErrorHandler.ts
+++ src/api/middlewares/GlobalErrorHandler.ts
@@ -1,15 +1,18 @@
 import { Request, Response } from 'express';
 import { Middleware, ExpressErrorMiddlewareInterface, HttpError, InternalServerError } from 'routing-controllers';
+import { Logger, LoggerInterface } from 'src/decorators/Logger';
 import { Service } from 'typedi';
 
 @Middleware({ type: 'after' })
 @Service()
 export class GlobalErrorHandler implements ExpressErrorMiddlewareInterface {
+    constructor(@Logger(__filename) private log: LoggerInterface) { }
     error(error: any, request: Request, response: Response, next: (err: any) => any) {
         if (error instanceof HttpError) {
             response.status(error.httpCode).send(error);
         } else {
             response.status(500).send(new InternalServerError("Unhandled Error"));
+            this.log.error('UnhandledError: ', error);
         }
     }
 }
